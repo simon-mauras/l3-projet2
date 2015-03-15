166d15d292e3e0861bcaf494f9406324c2c1a9ea
Reventlov <remy.grunblatt@ens-lyon.fr>
2015-03-15 14:02:21 +0100

    Changement d'indentation

--- src/checker.ml
+++ src/checker.ml
@@ -3,37 +3,37 @@
 (** Affiche sur la sorte founie les messages d'erreur et retourne une formule valide *)
 let check out (nb_vars, nb_clauses, clauses) =
 
-    (* Vérifie que les variables sont bien numérotées entre 1 et nb_vars *)
-    let max0 x = abs x in
-    let rec max1 = function
-      | [] -> 0 | x::l -> max (max0 x) (max1 l) in
-    let rec max2 = function
-      | [] -> 0 | x::l -> max (max1 x) (max2 l) in
-    let max_vars = max2 clauses in
-    let v = max max_vars nb_vars in
-    if max_vars > nb_vars
-      then Printf.fprintf out "Warning: variable ids are expected to be between 1 and %d (X%d found).\n" nb_vars v;
+  (* Vérifie que les variables sont bien numérotées entre 1 et nb_vars *)
+  let max0 x = abs x in
+  let rec max1 = function
+    | [] -> 0 | x::l -> max (max0 x) (max1 l) in
+  let rec max2 = function
+    | [] -> 0 | x::l -> max (max1 x) (max2 l) in
+  let max_vars = max2 clauses in
+  let v = max max_vars nb_vars in
+  if max_vars > nb_vars
+  then Printf.fprintf out "Warning: variable ids are expected to be between 1 and %d (X%d found).\n" nb_vars v;
 
-    (* Vérifie que le  nombre de clauses est correct *)
-    let c = List.length clauses in
-    if c <> nb_clauses
-      then Printf.fprintf out "Warning: wrong number of clauses (%d found, %d expected).\n" c nb_clauses;
-    
-    (* Suppression des clauses triviales *)
-    let nbTrivial = ref 0 in
-    let tabLiterals = Array.make (2*v+1) (-1) in
-    let act = ref 0 in
-    let cl = List.filter (fun l ->
-                            act := !act + 1;
-                            let trivial = ref false in
-                            List.iter (fun i ->
-                                         if tabLiterals.(v - i) = !act
-                                           then trivial := true;
-                                         tabLiterals.(v + i) <- !act) l;
-                            if !trivial
-                              then nbTrivial := !nbTrivial + 1;
-                            not !trivial) clauses in
-    if !nbTrivial > 0
-      then Printf.fprintf out "Info: %d trivial clause(s) deleted.\n" !nbTrivial;
-    
-    (v, List.length cl, cl)
+  (* Vérifie que le  nombre de clauses est correct *)
+  let c = List.length clauses in
+  if c <> nb_clauses
+  then Printf.fprintf out "Warning: wrong number of clauses (%d found, %d expected).\n" c nb_clauses;
+
+  (* Suppression des clauses triviales *)
+  let nbTrivial = ref 0 in
+  let tabLiterals = Array.make (2*v+1) (-1) in
+  let act = ref 0 in
+  let cl = List.filter (fun l ->
+      act := !act + 1;
+      let trivial = ref false in
+      List.iter (fun i ->
+          if tabLiterals.(v - i) = !act
+          then trivial := true;
+          tabLiterals.(v + i) <- !act) l;
+      if !trivial
+      then nbTrivial := !nbTrivial + 1;
+      not !trivial) clauses in
+  if !nbTrivial > 0
+  then Printf.fprintf out "Info: %d trivial clause(s) deleted.\n" !nbTrivial;
+
+  (v, List.length cl, cl)
--- src/formula_wl.ml
+++ src/formula_wl.ml
@@ -5,20 +5,20 @@ module Make (L: Sigs.Literal_type) : Sigs.Formula_type =
 struct
 
   module Literal = L
-  
+
   (** Informations sur les clauses. Le tableau contient pour chaque clause :
-  - La liste des litéraux de la clause
-  - Les itéraux surveillés dans cette clause (si sa taille est >= 2) *)
+      - La liste des litéraux de la clause
+      - Les itéraux surveillés dans cette clause (si sa taille est >= 2) *)
   type clausesArray = (Literal.t list * Literal.t option * Literal.t option) array
-  
+
   (** Information sur les litéraux surveillés.
-  Le tableau contient pour chaque literal la liste des clauses dans lesquelles il est surveillé *)
+      Le tableau contient pour chaque literal la liste des clauses dans lesquelles il est surveillé *)
   type watchedLiteralsArray = int list array
-  
+
   (** Informations sur les litéraux.
-  Le tableau contient pour chaque litéral un booléen étant vrai ssi le litéral a été mis à vrai *)
+      Le tableau contient pour chaque litéral un booléen étant vrai ssi le litéral a été mis à vrai *)
   type literalsArray = bool array
-  
+
   (** Réponses aux différentes requetes. Cela permet d'éviter un grand nombre de calculs redondants *)
   type requestAnswer = {
     mutable unitClauses : Literal.t list;
@@ -40,56 +40,56 @@ struct
       let vx = nbOccur.(Literal.id_of_literal x) + nbOccur.(Literal.id_of_literal (Literal.neg x)) in
       let vy = nbOccur.(Literal.id_of_literal y) + nbOccur.(Literal.id_of_literal (Literal.neg y)) in
       vx - vy in
-    
+
     let watchedLiterals = Array.make (2*nb_vars) [] in
-    
+
     let tabLiterals = Array.make (2*nb_vars) false in
-    
+
     let answers = {
       unitClauses = [];
       pureLiterals = [];
       freeLiterals = [];
       isFalse = false
     } in
-    
+
     let tabClauses = Array.of_list (List.mapi (fun i l ->
-                                                 let lst = List.map (fun a ->
-                                                                       let x = Literal.make a in
-                                                                       nbOccur.(Literal.id_of_literal x) <- nbOccur.(Literal.id_of_literal x) + 1;
-                                                                       x) l in
-                                                 match lst with
-                                                   | [] ->
-                                                     answers.isFalse <- true;
-                                                     (lst, None, None)
-                                                   | x::[] ->
-                                                     answers.unitClauses <- x::answers.unitClauses;
-                                                     watchedLiterals.(Literal.id_of_literal x) <- i::watchedLiterals.(Literal.id_of_literal x);
-                                                     (lst, Some x, None)
-                                                   | x::y::_->
-                                                     watchedLiterals.(Literal.id_of_literal x) <- i::watchedLiterals.(Literal.id_of_literal x);
-                                                     watchedLiterals.(Literal.id_of_literal y) <- i::watchedLiterals.(Literal.id_of_literal y);
-                                                     (lst, Some x, Some y)) clauses) in
-                                               
+        let lst = List.map (fun a ->
+            let x = Literal.make a in
+            nbOccur.(Literal.id_of_literal x) <- nbOccur.(Literal.id_of_literal x) + 1;
+            x) l in
+        match lst with
+        | [] ->
+          answers.isFalse <- true;
+          (lst, None, None)
+        | x::[] ->
+          answers.unitClauses <- x::answers.unitClauses;
+          watchedLiterals.(Literal.id_of_literal x) <- i::watchedLiterals.(Literal.id_of_literal x);
+          (lst, Some x, None)
+        | x::y::_->
+          watchedLiterals.(Literal.id_of_literal x) <- i::watchedLiterals.(Literal.id_of_literal x);
+          watchedLiterals.(Literal.id_of_literal y) <- i::watchedLiterals.(Literal.id_of_literal y);
+          (lst, Some x, Some y)) clauses) in
+
     answers.freeLiterals <- List.sort compare (literalList nb_vars);
-    
+
     (tabClauses, watchedLiterals, tabLiterals, answers)
-    
+
 
   (** Affiche la formule et divers informations associées sur la sortie out *)
   let print out (clauses, watchedLiterals, literals, answers) =
     let f x = Literal.print out x; output_string out " " in
     output_string out "----------- Clauses ------------\n";
     Array.iter (function l, a, b ->
-                  List.iter (fun x ->
-                               if a = Some x || b = Some x then begin
-                                 output_string out "[";
-                                 Literal.print out x;
-                                 output_string out "] ";
-                               end else begin
-                                 Literal.print out x;
-                                 output_string out " ";
-                               end) l;
-                  output_string out "\n") clauses;
+        List.iter (fun x ->
+            if a = Some x || b = Some x then begin
+              output_string out "[";
+              Literal.print out x;
+              output_string out "] ";
+            end else begin
+              Literal.print out x;
+              output_string out " ";
+            end) l;
+        output_string out "\n") clauses;
     output_string out "--------- unitClauses ----------\n";
     List.iter f answers.unitClauses;
     output_string out "\n--------- pureLiterals ---------\n";
@@ -98,52 +98,52 @@ struct
     List.iter f answers.freeLiterals;
     output_string out "\n----------- isFalse ------------\n";
     if answers.isFalse
-      then output_string out "Formula is false"
-      else output_string out "Can't assert that formula is false";
+    then output_string out "Formula is false"
+    else output_string out "Can't assert that formula is false";
     output_string out "\n--------------------------------\n"
 
   (** Ajoute à la formule l'hypothèse que le litéral x soit vrai *)
   let setLiteral x (clauses, watchedLiterals, literals, answers) =
     literals.(Literal.id_of_literal x) <- true;
-    
+
     let rec remove = function
       | [] -> []
       | a::l when a = x -> l
       | a::l when a = Literal.neg x -> l
       | a::l -> a::(remove l) in
     answers.freeLiterals <- remove answers.freeLiterals;
-    
+
     let find cl = match cl with
       | _,None,_ -> None
       | _,_,None -> None
       | lst, Some u, Some v ->
         let rec aux = (function
-           | [] -> None
-           | a::l -> if literals.(Literal.id_of_literal (Literal.neg a)) || a = u || a = v
-                       then aux l
-                       else Some a)
+            | [] -> None
+            | a::l -> if literals.(Literal.id_of_literal (Literal.neg a)) || a = u || a = v
+              then aux l
+              else Some a)
         in aux lst
     in
-                                  
+
     let old = Some (Literal.neg x) in
     let update i =
       let l, u, v = clauses.(i) in
       let other = if old = u then v else u in
       match find clauses.(i) with
-        | Some a ->
-          watchedLiterals.(Literal.id_of_literal a) <- i::watchedLiterals.(Literal.id_of_literal a);
-          clauses.(i) <- (l, Some a, other);
-          false (* On change le pointeur, car un autre a ete trouve *)
-        | None ->
-          (match other with
-             | None -> answers.isFalse <- true
-             | Some a ->
-               if literals.(Literal.id_of_literal a)
-                 then () (* La clause est daje satisfaite *)
-                 else if literals.(Literal.id_of_literal (Literal.neg a))
-                        then answers.isFalse <- true (* La clause est fausse *)
-                        else answers.unitClauses <- a::answers.unitClauses); (* Il reste une unique manière pour satisfaire la clause *)
-          true (* On garde le pointeur actuel car on en a pas trouve d'autre... *)
+      | Some a ->
+        watchedLiterals.(Literal.id_of_literal a) <- i::watchedLiterals.(Literal.id_of_literal a);
+        clauses.(i) <- (l, Some a, other);
+        false (* On change le pointeur, car un autre a ete trouve *)
+      | None ->
+        (match other with
+         | None -> answers.isFalse <- true
+         | Some a ->
+           if literals.(Literal.id_of_literal a)
+           then () (* La clause est daje satisfaite *)
+           else if literals.(Literal.id_of_literal (Literal.neg a))
+           then answers.isFalse <- true (* La clause est fausse *)
+           else answers.unitClauses <- a::answers.unitClauses); (* Il reste une unique manière pour satisfaire la clause *)
+        true (* On garde le pointeur actuel car on en a pas trouve d'autre... *)
     in
     watchedLiterals.(Literal.id_of_literal (Literal.neg x)) <-
       List.filter update watchedLiterals.(Literal.id_of_literal (Literal.neg x))
@@ -165,7 +165,7 @@ struct
       answers.unitClauses <- l;
       getUnitClause (clauses, watchedLiterals, literals, answers)
     | x::l -> Some x
-    
+
   (** Renvoie un litéral dont la négation est absente de la formule (sous les hypothèses actuelles) *)
   (* Les litéraux surveillés ne sont pas implémentés dans la version actuelle (la liste est toujours vide) *)
   let rec getPureLiteral (clauses, watchedLiterals, literals, answers) = match answers.pureLiterals with
@@ -174,7 +174,7 @@ struct
       answers.pureLiterals <- l;
       getPureLiteral (clauses, watchedLiterals, literals, answers)
     | x::l -> Some x
-    
+
   (** Renvoie un litéral sur lequel aucune hypothèse n'a été faite. *)
   let getFreeLiteral (_,_,_,answers) = match answers.freeLiterals with
     | [] -> None
