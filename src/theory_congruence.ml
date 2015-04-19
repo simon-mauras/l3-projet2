f1c636f1d0fcb10c04a48ac4b7fe9a3291309cca
Simon Mauras <simon.mauras@ens-lyon.fr>
2015-04-19 14:12:39 +0200

    Congruence presque ok (todo : verifier cl + congruence)

--- src/theory_congruence.ml
+++ src/theory_congruence.ml
@@ -15,8 +15,8 @@ module Make : Sigs.Theory_type =
         type t = Eq of atom * atom | Neq of atom * atom
         
         let neg = function
-          | Eq (a, b) -> Neq (a, b)
-          | Neq (a, b) -> Eq (a, b)
+        | Eq (a, b) -> Neq (a, b)
+        | Neq (a, b) -> Eq (a, b)
         
         let make x =
           let module S = Sigs.Data.Congruence in
@@ -41,18 +41,18 @@ module Make : Sigs.Theory_type =
         
         let print output x =
           let rec print_atom = function
-            | X s -> output_string output s
-            | Fun (s, l) -> output_string output s;
-                            List.iter (fun u -> output_string output "[";
-                                                print_atom u;
-                                                output_string output "]") l in
+          | X s -> output_string output s
+          | Fun (s, l) -> output_string output s;
+                          List.iter (fun u -> output_string output "[";
+                                              print_atom u;
+                                              output_string output "]") l in
           match x with
-            | Eq (a, b)  -> print_atom a;
-                            output_string output " = ";
-                            print_atom b
-            | Neq (a, b) -> print_atom a;
-                            output_string output " != ";
-                            print_atom b
+          | Eq (a, b)  -> print_atom a;
+                          output_string output " = ";
+                          print_atom b
+          | Neq (a, b) -> print_atom a;
+                          output_string output " != ";
+                          print_atom b
       end
     
     module MapVar = Map.Make (struct
@@ -62,10 +62,18 @@ module Make : Sigs.Theory_type =
     
     (** Type d'un ensemble de contraintes *)
     type t_literal = T.t option array
-    type t_var = int MapVar.t * T.atom array
+    type t_var = int MapVar.t * T.atom array * int list array
     type t_equality = int array * L.t option array * int array * (int * int) list array
     type t_disequality = (int * int * L.t) list ref
     type t = t_literal * t_var * t_equality * t_disequality
+    
+    let uniq l =
+      let cmp = Pervasives.compare in
+      let rec remove res = function
+      | a::b::l when cmp a b = 0 -> remove (res) (b::l)
+      | a::l -> remove (a::res) (l)
+      | [] -> res in
+      remove [] (List.sort cmp l)
     
     (** Un ensemble de contraintes vide *)
     let make tab =
@@ -74,64 +82,109 @@ module Make : Sigs.Theory_type =
       let tabLiterals = Array.make nbLiterals None in
       for i=1 to (Array.length tab) - 1 do
         match tab.(i) with
-          | None -> ()
-          | Some x ->
-            tabLiterals.(L.id_of_literal (L.make i)) <- Some x;
-            tabLiterals.(L.id_of_literal (L.make (-i)))<- Some (T.neg x);
+        | None -> ()
+        | Some x ->
+          tabLiterals.(L.id_of_literal (L.make i)) <- Some x;
+          tabLiterals.(L.id_of_literal (L.make (-i)))<- Some (T.neg x);
       done;
       
       let nbVars = ref 0 in
       let mapVars = ref MapVar.empty in
-      let addVars x =
+      let rec addVars x = 
         if not (MapVar.mem x !mapVars) then begin
           mapVars := MapVar.add x !nbVars !mapVars;
           incr nbVars;
-        end in
+        end;
+        match x with
+        | T.Fun(s, l) -> List.iter addVars l 
+        | _ -> ()
+        in
       let findVars = function
-        | T.Eq(a, b) -> addVars a; addVars b
-        | T.Neq(a, b) -> addVars a; addVars b in
+      | T.Eq(a, b) -> addVars a; addVars b
+      | T.Neq(a, b) -> addVars a; addVars b in
       Array.iter (function None -> () | Some x -> findVars x) tab;
       let tabVars = Array.make !nbVars (T.X "") in
       MapVar.iter (fun s i -> tabVars.(i) <- s) !mapVars;
+      let useLists = Array.make !nbVars [] in
+      let rec buildUseLists x = 
+        let id = MapVar.find x !mapVars in
+        match x with
+        | T.X s -> id
+        | T.Fun (s,l) -> List.iter (fun y -> let i = buildUseLists y in
+                                             useLists.(i) <- id::useLists.(i)) l; id in
+      MapVar.iter (fun s i -> ignore (buildUseLists s)) !mapVars;
+      Array.iteri (fun i l -> useLists.(i) <- uniq l) useLists;
       
+      (*
+      let rec print_atom = function
+      | T.X s -> print_string s
+      | T.Fun (s, l) -> print_string s;
+                      List.iter (fun u -> print_string "[";
+                                          print_atom u;
+                                          print_string "]") l in
+      Array.iteri (fun i x -> print_atom x; print_string " "; List.iter (fun y -> print_atom tabVars.(y); print_string " ") useLists.(i); print_newline ()) tabVars;
+      *)
+          
       ((tabLiterals),
-       (!mapVars, tabVars),
+       (!mapVars, tabVars, useLists),
        (Array.init !nbVars (fun i -> i), Array.make !nbVars None, Array.make !nbVars 1, Array.make nbLiterals []),
        ref [])
     
         
     (** Ajoute une contrainte *)
-    let setConstraint lit (tabLiterals, (mapVars, tabVars), (find, cause, size, equality), disequality) =
+    let setConstraint lit (tabLiterals, (mapVars, tabVars, useLists), (find, cause, size, equality), disequality) =
+      let idLit = L.id_of_literal lit in
+      
       let rec root i =
         if i = find.(i)
           then i
           else root find.(i) in
-      let idLit = L.id_of_literal lit in
+          
+      let union u v =
+        size.(u) <- size.(u) + size.(v);
+        find.(v) <- u;
+        cause.(v) <- Some lit;
+        equality.(idLit) <- (v, u)::equality.(idLit) in
+        
+      let rec merge a b =
+        let u = root a in
+        let v = root b in
+        if u <> v then begin
+          if size.(u) > size.(v)
+            then union u v
+            else union v u;
+          let signature i = match tabVars.(i) with
+          | T.X s -> failwith "Impossible"
+          | T.Fun (s, l) -> ((s, List.map (fun x -> root (MapVar.find x mapVars)) l), i) in
+          let cmp = Pervasives.compare in
+          let rec aux = function
+          | [], _ -> ()
+          | _, [] -> ()
+          | (x1, i1)::l1, (x2, i2)::l2 ->
+            if cmp x1 x2 = 0 then begin
+              merge i1 i2;
+              aux (l1, l2);
+            end else if cmp x1 x2 < 0
+              then aux (l1,((x2, i2)::l2))
+              else aux (((x1, i1)::l1),l2) in
+          let aSigs = List.sort cmp (List.map signature useLists.(u)) in
+          let bSigs = List.sort cmp (List.map signature useLists.(v)) in
+          aux (aSigs,bSigs) 
+        end in
+      
       match tabLiterals.(idLit) with
       | Some x -> begin
         match x with
-        | T.Eq(a, b) ->
-          let u = root (MapVar.find a mapVars) in
-          let v = root (MapVar.find b mapVars) in
-          if u <> v then begin
-            if size.(u) > size.(v) then begin
-              size.(u) <- size.(u) + size.(v);
-              find.(v) <- u;
-              cause.(v) <- Some lit;
-              equality.(idLit) <- [(v, u)];
-            end else begin
-              size.(v) <- size.(u) + size.(v);
-              find.(u) <- v;
-              cause.(u) <- Some lit;
-              equality.(idLit) <- [(u, v)];
-            end
-          end
+        | T.Eq(a, b) -> let u = MapVar.find a mapVars in
+                        let v = MapVar.find b mapVars in
+                        equality.(idLit) <- [];
+                        merge u v;
         | T.Neq(a, b) -> disequality := ((MapVar.find a mapVars), (MapVar.find b mapVars), lit)::!disequality
         end
       | None -> ()
     
     (** Oublie une contrainte *)
-    let forgetConstraint lit (tabLiterals, (mapVars, tabVars), (find, cause, size, equality), disequality) =
+    let forgetConstraint lit (tabLiterals, (mapVars, tabVars, useLists), (find, cause, size, equality), disequality) =
       let idLit = L.id_of_literal lit in
       match tabLiterals.(idLit) with
       | Some x -> begin
@@ -147,7 +200,7 @@ module Make : Sigs.Theory_type =
       | None -> ()
     
     (** Renvoie une eventuelle contradiction entre les contraintes actuelles *)
-    let getContradiction (tabLiterals, (mapVars, tabVars), (find, cause, size, equality), disequality) =
+    let getContradiction (tabLiterals, (mapVars, tabVars, useLists), (find, cause, size, equality), disequality) =
     
       let rec root i =
         if i <> find.(i)
